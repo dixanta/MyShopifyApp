b9bb66ca3d9438bad57cc25edb8e52d638015cb4
Dixanta Bahadur Shrestha <dixanta@gmail.com>
2017-10-10 21:27:04 +0545

    Update AppConstant.java

--- src/main/java/com/lfa/myshopify/constant/AppConstant.java
+++ src/main/java/com/lfa/myshopify/constant/AppConstant.java
@@ -10,7 +10,7 @@ package com.lfa.myshopify.constant;
  * @author USER
  */
 public class AppConstant {
-    public static final String ClientId = "1545ed24dd1e530be88dedb91965e0bb";
-    public static final String ClientSecret="e3c621cd829bf95c213ae0b00232e72b";
+    public static final String ClientId = "";
+    public static final String ClientSecret="";
     public static final String SHOPIFY_SCOPES="read_customers,write_customers,read_products,read_orders,read_content,read_themes,read_script_tags";
 }
